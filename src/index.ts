cadb2b78836c8bc25b966f1124f8bcf2bffc10ca
Justin Bird <justin@thewordisbird.dev>
2022-12-03 10:57:27 -0800

    day 3 solution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import { day_1_problem_1 } from "./day-1";
-import { day_2_problem_1, day_2_problem_2 } from "./day-2";
-day_2_problem_2();
+import { day_1_problem_1 } from './day-1';
+import { day_2_problem_1, day_2_problem_2 } from './day-2';
+import { partOneSolution, partTwoSolution } from './day-3';
 
-// 4653 too low
+partOneSolution().then((result) => console.log('Part One: ', result));
+partTwoSolution().then((result) => console.log('Part Two: ', result));
+
+// 3975 too high
