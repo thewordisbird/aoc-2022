b34dd93cdcd2eaf12ee5acc53895c05d523e859d
Justin Bird <justin@thewordisbird.dev>
2022-12-04 17:21:45 -0800

    created readLine and groupBy async generators

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import { day_1_problem_1 } from './day-1';
 import { day_2_problem_1, day_2_problem_2 } from './day-2';
 import { partOneSolution, partTwoSolution } from './day-3';
 import { d4SolutionA, d4SolutionB } from './day-4';
+import { main } from './utils/parse';
 
-console.log('question 1:');
-d4SolutionA().then((score) => console.log(score));
-console.log('question 2:');
-d4SolutionB().then((score) => console.log(score));
+// console.log('question 1:');
+// d4SolutionA().then((score) => console.log(score));
+// console.log('question 2:');
+// d4SolutionB().then((score) => console.log(score));
+
+main();
 
 // 549 too high
 // 522 too high
