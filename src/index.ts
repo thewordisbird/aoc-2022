a1bbe240746925f8d099b6f9084073cae3fadac3
Justin Bird <justin@thewordisbird.dev>
2022-12-04 21:11:09 -0800

    update entrypoint for async formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import { day1Problem1, day1problem2 } from './day-1';
+import { day1Problem1, day1Problem2 } from './day-1';
+import { day2Problem1, day2Promblem2 } from './day-2';
 
-logResults(1, day1Problem1, day1problem2);
+async function main() {
+  await logDayResult(1, day1Problem1, day1Problem2);
+  await logDayResult(2, day2Problem1, day2Promblem2);
+}
 
-async function logResults(
+main();
+
+/* helpers */
+
+async function logDayResult(
   day: number,
-  solution1: () => Promise<unknown>,
-  solution2: () => Promise<unknown>
+  solution1?: () => Promise<unknown>,
+  solution2?: () => Promise<unknown>
 ) {
   console.log(`Advent of Code - Day ${day}`);
-  await solution1().then((result) => console.log('Problem 1: ', result));
-  await solution2().then((result) => console.log('Problem 2: ', result));
+  solution1 &&
+    (await solution1().then((result) => console.log('Problem 1:', result)));
+  solution2 &&
+    (await solution2().then((result) => console.log('Problem 2: ', result)));
   console.log('\n');
 }
